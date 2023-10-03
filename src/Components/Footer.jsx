76dbbfd931164b11013c3eefa0936731af684f47
javlav98 <jrlavin98@gmail.com>
2023-10-02 22:40:59 -0400

    Dont remember

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,35 +1,9 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Footer.css';
+import React from 'react'
 
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer>
-      <div className="footerNav">
-      <ul className='ulFooter'>
-          <li className="footer-item">
-            <NavLink to="/"> Home </NavLink>
-          </li>
-          <li className="footer-item">
-            <NavLink to="/about"> About</NavLink>
-          </li>
-          <li className="footer-item">
-            <NavLink to="/schedule"> Schedule </NavLink>
-          </li>
-          <li className="footer-item">
-            <NavLink to="/djs"> Djs </NavLink>
-          </li>
-          <li className="footer-item">
-            <NavLink to="/contact"> Contact</NavLink>
-          </li> 
-          <li className="footer-item">
-            <NavLink to="/donate"> Donate </NavLink>
-          </li>
-        </ul>
-        <p className='Copyrights'>© 2023 Noiszer</p>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+    <div>
+      
+    </div>
+  )
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,73 +1,70 @@
 import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { FaBars, FaPlay, FaPause, FaTimes } from 'react-icons/fa'; // Import play, pause, menu, and close icons
-import Marquee from './marquee';
-
+import { Link } from 'react-router-dom';
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import SearchBar from './Searchbar'; // Import your SearchBar component
 import './Navbar.css';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false); // Track play/pause state
-  const streamUrl = 'your_radio_stream_url'; // Replace with actual stream URL
+  const [menuActive, setMenuActive] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
+    setMenuActive(!menuActive);
   };
 
-  const togglePlayPause = () => {
-    // Add logic here to play or pause audio based on the isPlaying state
-    // Toggle the isPlaying state accordingly
-    setIsPlaying(!isPlaying);
+  const closeMenu = () => {
+    setIsOpen(false);
+    setMenuActive(false);
   };
 
   return (
     <div>
-      <Marquee />
       <nav className="navbar">
         <div className="navbar-container">
-          <div className="menu-icon" onClick={toggleMenu}>
-            {isOpen ? <FaTimes /> : <FaBars />}
-          </div>
           <Link to="/" className="navbar-logo">
-            noiszer
+            <h1>noiszer</h1>
           </Link>
-          <div className="toolnav" onClick={togglePlayPause}>
-            {isPlaying ? <FaPause /> : <FaPlay />}
+
+          <SearchBar /> {/* Add your SearchBar component here */}
+          
+          <div className="menu-icon" onClick={toggleMenu}>
+            {menuActive ? <AiOutlineClose /> : <AiOutlineMenu />}
           </div>
+          <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
+            <li className="nav-item">
+              <Link to="/" onClick={closeMenu}>
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" onClick={closeMenu}>
+                ABOUT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/schedule" onClick={closeMenu}>
+                SCHEDULE
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/djs" onClick={closeMenu}>
+                DJ'S
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" onClick={closeMenu}>
+                CONTACT
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/donate" onClick={closeMenu}>
+                DONATE
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
-      <ul className={`nav-menu ${isOpen ? 'active' : ''}`}>
-        <li className="nav-item">
-          <NavLink to="/" exact activeClassName="active" onClick={toggleMenu}>
-            home
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/about" activeClassName="active" onClick={toggleMenu}>
-            about
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/schedule" activeClassName="active" onClick={toggleMenu}>
-            schedule
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/djs" activeClassName="active" onClick={toggleMenu}>
-            dj's
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/contact" activeClassName="active" onClick={toggleMenu}>
-            contact
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink to="/donate" activeClassName="active" onClick={toggleMenu}>
-            donate
-          </NavLink>
-        </li>
-      </ul>
     </div>
   );
 };
